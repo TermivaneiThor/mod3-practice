651840bc69decbe4da875d9364d975c67407bd33
Iván Manceras Miranda <mancerasmirandaivan@gmail.com>
2023-10-29 16:02:14 +0100

    Exercise 7 Complete

--- src/js/exercises/exercise-7.js
+++ src/js/exercises/exercise-7.js
@@ -6,3 +6,22 @@
 * texto y el resultado
 */
 
+function esPalindromo(cadena) {
+    //* Eliminamos los espacios y convertimos la cadena a minúsculas para hacer la comparación más precisa.
+    let cadenaLimpia = cadena.toLowerCase().replace(/\s/g, '');
+  
+    //* Invertimos la cadena para compararla con la original.
+    let cadenaInvertida = cadenaLimpia.split('').reverse().join('');
+  
+    //* Comparamos la cadena original con la cadena invertida para determinar si es un palíndromo.
+    if (cadenaLimpia === cadenaInvertida) {
+      return `${cadena} es un palíndromo.`;
+    } else {
+      return `${cadena} no es un palíndromo.`;
+    }
+  }
+  
+  //* Ejemplo de uso:
+  let resultado = esPalindromo("Anilina");
+  showContent(7, resultado); //* Output: "Anilina es un palíndromo."
+  
